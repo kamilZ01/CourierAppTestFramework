eb9acde7dbb1904e4ac9fa42fb5fd341c47f0b93
kamilZ <zarychkamil@gmail.com>
2025-02-09 00:44:40 +0100

    Add configuration to run Cucumber tests using RunTest class

--- src/test/java/org/automation/framework/testrunners/RunTest.java
+++ src/test/java/org/automation/framework/testrunners/RunTest.java
@@ -1,4 +1,12 @@
 package org.automation.framework.testrunners;
 
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+//@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report/cucumber.html,json:target/cucumber-report.json")
 public class RunTest {
 }
